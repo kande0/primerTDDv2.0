6de1b14e79410ee5cd927f81431f2290c37537bf
Carlos <carlosblascoesapada@gmail.com>
2023-04-07 22:43:06 +0200

    Primer método de prueba

--- src/TestsBasicos.java
+++ src/TestsBasicos.java
@@ -2,14 +2,12 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import junit.framework.Assert;
-
 public class TestsBasicos {
 
 	@Test
 	public void Una_nueva_calculadora_debe_iniciar_en_cero() {
 		Calculadora calc = new Calculadora();
-		Assert.assertEquals(0, calc.Valor());
+		assertEquals(0, calc.Valor());
 	}
 
 }
